d72ad424942d494fbe7b421c8e0133a9d21189f6
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-08-26 06:44:08 +0000

    Automatically backed up by Learn

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -5,7 +5,17 @@ class BandInput extends Component {
   render() {
     return(
       <div>
-        Band Input
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            type="text"
+            name="name"
+            onChange={this.handleChange}
+            value={this.state.name}
+          />
+          <input 
+            type="submit"
+          />
+        </form>
       </div>
     )
   }
